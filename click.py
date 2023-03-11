b8092ecb6443c13a87505178bc50f139f8c95eeb
Ayobami Bamigboye <ayobamibamigboye@Ayobamis-MBP.lan>
2023-03-11 16:00:00 +0100

    init main()

--- click.py
+++ click.py
@@ -27,7 +27,7 @@ def load_os_env(path):
     secrets['PASSWORD'] = os.getenv('CLICKER_PASSWORD')
     return secrets
 
-def domain_pages(url):
+def domain_pages(url, max_page_number, opportunities, total_found, target_search_list):
     # generate a dict of the title mapped to urls of opportunities for the cartegory url passed in 
     
     page = requests.get(url)
@@ -37,34 +37,36 @@ def domain_pages(url):
     page_array = [int(pages[0].find("span", {"class": "current"}).text), int(pages[0].find_all('a')[2].get('title'))]
     
     
+    
     if page_array[1] > max_page_number:
         for page in range(page_array[0], max_page_number + 1):
-            get_opportunities(url, page)
+            get_opportunities(url, page, opportunities, total_found, target_search_list)
     else:
         for page in range(page_array[0], page_array[1]):
-            get_opportunities(url, page)
+            get_opportunities(url, page, opportunities, total_found, target_search_list)
             
     return
 
-def get_opportunities(url, page):
+def get_opportunities(url, page, opportunities, total_found, target_search_list):
     # parse the list of opportunities from the provided page
     curated_url = url+"page/"+str(page)+"/"
     page = requests.get(url+"page"+str(page))
     soup = BeautifulSoup(page.content, "html.parser")
     
-    return parse_opportunities(soup)
+    return parse_opportunities(soup, opportunities, total_found, target_search_list)
 
-def parse_opportunities(soup):
+def parse_opportunities(soup, opportunities, total_found, target_search_list):
+    
     # parse the list opportunities from the provided BeautifulSoup Object
     results = soup.find_all("h3", class_="entry-title td-module-title")
 
     for result in tqdm(results):
         opportunities[result.find('a').get('title')] = result.find('a').get('href')
-        total_found.append(investigate_url_content(result.find('a').get('href')))
+        total_found.append(investigate_url_content(result.find('a').get('href'), target_search_list))
               
     return
 
-def investigate_url_content(url):
+def investigate_url_content(url, target_search_list):
     # search for target_search_list occurence in the url
     # if string(s) occur, append it to the df
     text = ""
@@ -89,7 +91,9 @@ def send_to_email(df, s):
     emaillist = [elem.strip().split(',') for elem in s['RECEPIENTS']]
     msg = MIMEMultipart()
     msg['Subject'] = "Top Picks: " + str(date.today())
-    msg['From'] = "ayobama72@gmail.com"
+    msg['From'] = s['FROM'][0]
+    
+    print(s)
 
 
     html = """\
@@ -112,62 +116,61 @@ def send_to_email(df, s):
         
     return
 
-# url setup
-url = {}
+def main():
+    """
+    Supported URLs for Opportunity for Africans
 
-# url['base_url'] = "https://www.opportunitiesforafricans.com/"
-url['fellowships_url'] = "https://www.opportunitiesforafricans.com/category/fellowships/"
-# url['internship_url'] = "https://www.opportunitiesforafricans.com/category/internships/"
-# url['scholarships_masters_url'] = "https://www.opportunitiesforafricans.com/category/scholarships/"
-# url['scholarships_postgraduate_url'] = "https://www.opportunitiesforafricans.com/category/scholarships/post-graduate/"
-# url['scholarships_undergraduate_url'] = "https://www.opportunitiesforafricans.com/category/scholarships/undergraduate/"
+    base_url = "https://www.opportunitiesforafricans.com/" #doesn't have page number, may not be very needed
+    fellowships_url = "https://www.opportunitiesforafricans.com/category/fellowships/"
+    internship_url = "https://www.opportunitiesforafricans.com/category/internships/"
+    scholarships_masters_url = "https://www.opportunitiesforafricans.com/category/scholarships/"
+    scholarships_postgraduate_url = "https://www.opportunitiesforafricans.com/category/scholarships/post-graduate/"
+    scholarships_undergraduate_url = "https://www.opportunitiesforafricans.com/category/scholarships/undergraduate/"
+    call_for_nominations_url = "https://www.opportunitiesforafricans.com/category/calls-for-nomination/" (not supported)
+    call_for_papers_url = "https://www.opportunitiesforafricans.com/category/calls-for-papers/"
+    call_for_proposals_url = "https://www.opportunitiesforafricans.com/category/call-for-proposals/"
+    awards_url = "https://www.opportunitiesforafricans.com/category/awards/"
 
-"""
-Supported URLs for Opportunity for Africans
+    How to filter target searches?
+    Search for terms or expressions?
+    Doctoral := PhD
+    Nigerian specific: 
+    All persons: 
+    STEM words:
+    Cloud Engineering:
+    Matrials Engineering: 
+    Generics: "application", "candidates"
+    """
+    
+    # url setup
+    url = {}
+    
+    # url['base_url'] = "https://www.opportunitiesforafricans.com/"
+    url['fellowships_url'] = "https://www.opportunitiesforafricans.com/category/fellowships/"
+    # url['internship_url'] = "https://www.opportunitiesforafricans.com/category/internships/"
+    # url['scholarships_masters_url'] = "https://www.opportunitiesforafricans.com/category/scholarships/"
+    # url['scholarships_postgraduate_url'] = "https://www.opportunitiesforafricans.com/category/scholarships/post-graduate/"
+    # url['scholarships_undergraduate_url'] = "https://www.opportunitiesforafricans.com/category/scholarships/undergraduate/"
 
-base_url = "https://www.opportunitiesforafricans.com/" #doesn't have page number, may not be very needed
-fellowships_url = "https://www.opportunitiesforafricans.com/category/fellowships/"
-internship_url = "https://www.opportunitiesforafricans.com/category/internships/"
-scholarships_masters_url = "https://www.opportunitiesforafricans.com/category/scholarships/"
-scholarships_postgraduate_url = "https://www.opportunitiesforafricans.com/category/scholarships/post-graduate/"
-scholarships_undergraduate_url = "https://www.opportunitiesforafricans.com/category/scholarships/undergraduate/"
-call_for_nominations_url = "https://www.opportunitiesforafricans.com/category/calls-for-nomination/" (not supported)
-call_for_papers_url = "https://www.opportunitiesforafricans.com/category/calls-for-papers/"
-call_for_proposals_url = "https://www.opportunitiesforafricans.com/category/call-for-proposals/"
-awards_url = "https://www.opportunitiesforafricans.com/category/awards/"
+    target_search_list = ["application", "candidates"]
 
-How to filter target searches?
-Search for terms or expressions?
-Doctoral := PhD
-Nigerian specific: 
-All persons: 
-STEM words:
-Cloud Engineering:
-Matrials Engineering: 
-Generics: "application", "candidates"
-"""
-target_search_list = ["application", "candidates"]
+    opportunities = {}
+    max_page_number = 1
+    total_found = []
 
-USERNAME = ""
-PASSWORD = ""
-RECEPIENTS = []
-FROM = ""
-SMTP_SERVER = ""
+    s = load_os_env("./koda/config.yaml")
 
-opportunities = {}
-max_page_number = 1
-total_found = []
+    print("URLs loaded:", list(url.keys()))
 
-s = load_os_env("./koda/config.yaml")
+    for val in tqdm(url.values()):
+        domain_pages(val, max_page_number, opportunities, total_found, target_search_list)
 
-print("URLs loaded:", list(url.keys()))
+    # convert to python.dataframe
+    df = pd.DataFrame(opportunities.items(), columns=['Opportunity','URL'])
+    df['target_found'] = total_found
 
-for val in tqdm(url.values()):
-    domain_pages(val)
-
-# convert to python.dataframe
-df = pd.DataFrame(opportunities.items(), columns=['Opportunity','URL'])
-df['target_found'] = total_found
-
-# send table as email
-send_to_email(df, s)
+    # send table as email
+    send_to_email(df, s)
+    
+if __name__ == "__main__":
+    main()
